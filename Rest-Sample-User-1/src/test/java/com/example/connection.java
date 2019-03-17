11f37bc0aaa59eb9206a1502119bd331d7276357
Aniket Ambadkar <aniket.ambadkar@novartis.com>
2019-03-17 16:12:42 +0530

    new connection file added to subbranch

---
+++ Rest-Sample-User-1/src/test/java/com/example/connection.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.emc.documentum.RestSampleUser1Application;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RestSampleUser1Application.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:8888")   // 4
+public class RestSampleUser1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+	
+	@Test
+	public void contextLoads() {
+		System.out.println("thisi is test code");
+	}
+	
+	@Test
+  public void getLogin() {
+		System.out.println("login done");
+	}
+  
+  @Test
+	public void masterLogin() {
+		System.out.println("this is master login code");
+	}
+	
+	@Test
+	public void masterLogin() {
+		System.out.println("this is branch update");
+	}
+	
+	@Test
+	public void subbranchLogin() {
+		System.out.println("this is subbranch update");
+	}
+}
